c341f8b88148a8bb6b2db61e7dcf6e7c5c4562e5
Igor Gomes Leandro <131786409+igorgleandro@users.noreply.github.com>
2025-10-10 22:58:11 +0100

    railway config

--- src/components/DefaultLayout.jsx
+++ src/components/DefaultLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import SidebarNav from "./SidebarNav";
-import Topbar from "./Topbar";
+//import {Topbar} from "./Topbar";
 
 
 export default function DefaultLayout() {
@@ -8,7 +8,7 @@ export default function DefaultLayout() {
         <div className="min-h-screen bg-gray-50 dark:bg-gray-900 grid grid-cols-[16rem_1fr]">
             <SidebarNav />
             <div className="flex min-h-screen flex-col">
-                <Topbar />
+
                 <main className="flex-1 p-4 bg-gray-50 dark:bg-gray-900">
                     <Outlet />
                 </main>
