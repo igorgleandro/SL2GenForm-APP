fb7db6f63f2cb594cd3b82a7a0d4ec4beeb9117a
Igor Gomes Leandro <131786409+igorgleandro@users.noreply.github.com>
2025-10-19 14:50:19 +0100

    Fixed tob and sidebar and signup page

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -47,7 +47,7 @@ export default function SignupPage() {
         setLoading(true);
 
         try {
-            const response = await fetch('${API_BASE_URL}/users', {
+            const response = await fetch(`${API_BASE_URL}/users`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
